e0a468b7c80e9ef828966109fbcaf5ac5c14d837
Kir <hokeygo@gmail.com>
2023-10-25 21:15:58 +0300

    fix code style

--- life-game/source/llvm-pass.cpp
+++ life-game/source/llvm-pass.cpp
@@ -9,64 +9,64 @@ using namespace llvm;
 #include <string>
 
 bool isFuncLogger(StringRef name) {
-    return name == "Logger" || name == "Dump";
-  }
+        return name == "Logger" || name == "Dump";
+    }
 
 namespace {
-  struct MyPass : public FunctionPass {
-    static char ID;
-    MyPass() : FunctionPass(ID) {}
+    struct MyPass : public FunctionPass {
+        static char ID;
+        MyPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      if(isFuncLogger(F.getName()) || F.getName().contains("randomFillCellField"))
-        return false;
+        virtual bool runOnFunction(Function &F) {
+            if(isFuncLogger(F.getName()) || F.getName().contains("randomFillCellField"))
+                return false;
 
-      LLVMContext &Ctx = F.getContext();
-      IRBuilder<> builder(Ctx);
-      Type *retType = Type::getVoidTy(Ctx);
+            LLVMContext &Ctx = F.getContext();
+            IRBuilder<> builder(Ctx);
+            Type *retType = Type::getVoidTy(Ctx);
 
-      ArrayRef<Type *> LoggerParamTypes = {builder.getInt8Ty()->getPointerTo()};
-      FunctionType *LoggerFuncType = FunctionType::get(retType, LoggerParamTypes, false);
-      FunctionCallee LoggerFunc = F.getParent()->getOrInsertFunction("Logger", LoggerFuncType);
+            ArrayRef<Type *> LoggerParamTypes = {builder.getInt8Ty()->getPointerTo()};
+            FunctionType *LoggerFuncType = FunctionType::get(retType, LoggerParamTypes, false);
+            FunctionCallee LoggerFunc = F.getParent()->getOrInsertFunction("Logger", LoggerFuncType);
 
-      ArrayRef<Type *> DumpParamTypes = {};
-      FunctionType *DumpFuncType = FunctionType::get(retType, DumpParamTypes, false);
-      FunctionCallee DumpFunc = F.getParent()->getOrInsertFunction("Dump", DumpFuncType);
+            ArrayRef<Type *> DumpParamTypes = {};
+            FunctionType *DumpFuncType = FunctionType::get(retType, DumpParamTypes, false);
+            FunctionCallee DumpFunc = F.getParent()->getOrInsertFunction("Dump", DumpFuncType);
 
-      for (auto &B : F) {
-        for (auto &I : B) {
-          if (auto *call = dyn_cast<CallInst>(&I))
-            if (call->getCalledFunction() && isFuncLogger(call->getCalledFunction()->getName()))
-              continue;
+            for (auto &B : F) {
+                for (auto &I : B) {
+                    if (auto *call = dyn_cast<CallInst>(&I))
+                        if (call->getCalledFunction() && isFuncLogger(call->getCalledFunction()->getName()))
+                            continue;
 
-          // std::string stream;
-          // raw_string_ostream instr_stream{stream};
-          // I.print(instr_stream, true);
+                    // std::string stream;
+                    // raw_string_ostream instr_stream{stream};
+                    // I.print(instr_stream, true);
 
-          builder.SetInsertPoint(&I);
-          // Value *funcName = builder.CreateGlobalStringPtr(F.getName());
-          // Value *instr = builder.CreateGlobalStringPtr(instr_stream.str());
-          Value* instrName = builder.CreateGlobalStringPtr(I.getOpcodeName());
-          Value *args[] = {instrName};
-          builder.CreateCall(LoggerFunc, args);
+                    builder.SetInsertPoint(&I);
+                    // Value *funcName = builder.CreateGlobalStringPtr(F.getName());
+                    // Value *instr = builder.CreateGlobalStringPtr(instr_stream.str());
+                    Value* instrName = builder.CreateGlobalStringPtr(I.getOpcodeName());
+                    Value *args[] = {instrName};
+                    builder.CreateCall(LoggerFunc, args);
 
-          if (auto *ret = dyn_cast<ReturnInst>(&I))
-            if (F.getName() == "main")
-              builder.CreateCall(DumpFunc, None);
+                    if (auto *ret = dyn_cast<ReturnInst>(&I))
+                        if (F.getName() == "main")
+                            builder.CreateCall(DumpFunc, None);
+                }
+            }
+
+            return true;
         }
-      }
-
-      return true;
-    }
-  };
+    };
 }
 
 char MyPass::ID = 0;
 
 static void registerMyPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new MyPass());
+                                                 legacy::PassManagerBase &PM) {
+    PM.add(new MyPass());
 }
 static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_OptimizerLast,
-                 registerMyPass);
+    RegisterMyPass(PassManagerBuilder::EP_OptimizerLast,
+                                 registerMyPass);
